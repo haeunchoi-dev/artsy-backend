215767698750fce559106da0f173ddb30d4fc97d
haeun.choi <haeunchoi.dev@gmail.com>
2023-08-14 14:07:51 +0900

    feat: add user-tickets, price api-docs

--- src/swagger-ui/docs/api/user/index.js
+++ src/swagger-ui/docs/api/user/index.js
@@ -2,10 +2,16 @@ import signUpWithEmail from './signUpWithEmail';
 import checkDuplicatedEmail from './checkDuplicatedEmail';
 import loginWithEmail from './loginWithEmail';
 import badge from './badge';
+import tickets from './tickets';
+import ticket from './ticket';
+import price from './price';
 
 export default {
   ...signUpWithEmail,
   ...checkDuplicatedEmail,
   ...loginWithEmail,
   ...badge,
+  ...tickets,
+  ...ticket,
+  ...price,
 };
