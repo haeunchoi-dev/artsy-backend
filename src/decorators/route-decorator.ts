3769b83b15282a5074e51294524fab5431298b75
haeun.choi <haeunchoi.dev@gmail.com>
2023-08-15 21:11:30 +0900

    Feat: dacorator 생성
    route와 di decorator 생성

---
+++ src/decorators/route-decorator.ts
@@ -0,0 +1,9 @@
+export function Route(method: string, path: string) {
+  return function (
+    target: any,
+    propertyKey: string,
+    // descriptor: PropertyDescriptor,
+  ) {
+    Reflect.defineMetadata('route', { method, path }, target, propertyKey);
+  };
+}
