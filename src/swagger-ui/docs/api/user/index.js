5be8d1eb5d8cdf26588c076c861fb9d73da64fc5
qpow1018 <56070196+qpow1018@users.noreply.github.com>
2023-08-15 18:10:15 +0900

    Remove: badge api-docs

--- src/swagger-ui/docs/api/user/index.js
+++ src/swagger-ui/docs/api/user/index.js
@@ -1,7 +1,6 @@
 import signUpWithEmail from './signUpWithEmail';
 import checkDuplicatedEmail from './checkDuplicatedEmail';
 import loginWithEmail from './loginWithEmail';
-import badge from './badge';
 import tickets from './tickets';
 import ticket from './ticket';
 import price from './price';
@@ -10,7 +9,6 @@ export default {
   ...signUpWithEmail,
   ...checkDuplicatedEmail,
   ...loginWithEmail,
-  ...badge,
   ...tickets,
   ...ticket,
   ...price,
