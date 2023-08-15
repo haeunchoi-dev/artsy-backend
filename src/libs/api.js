ef366564d62f3852af9412e633288e529aa900fa
haeun.choi <haeunchoi.dev@gmail.com>
2023-08-15 12:21:27 +0900

    Feat: libs/api에 defaultProcess를 생성
    error를 공통 처리

---
+++ src/libs/api.js
@@ -0,0 +1,9 @@
+export const defaultProcess = (requestHandler) => {
+  return async (req, res, next) => {
+    try {
+      await requestHandler(req, res, next);
+    } catch (error) {
+      next(error);
+    }
+  };
+};
