7ec97fc09386e7e05b6532f3d61cbafb947dc3d0
haeun.choi <haeunchoi.dev@gmail.com>
2023-08-16 16:45:34 +0900

    Feat: cors 조건 추가
    출처 허용, 사용자 인증 리소스 접근 허용

--- src/index.ts
+++ src/index.ts
@@ -9,7 +9,13 @@ import { ERRORS } from './error/errors';
 
 const app = express();
 app.use(logger('dev'));
-app.use(cors());
+app.use(
+  cors({
+    credentials: true, // 사용자 인증이 필요한 리소스(쿠키 ..등) 접근
+    //origin: 'http://localhost:5173',
+    origin: true, // 출처 허용 옵션
+  }),
+);
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
@@ -21,7 +27,7 @@ app.use('/api/api-docs', swaggerUI.serve, swaggerUI.setup(specs, setUpOption));
 
 app.use((err: any, req: Request, res: Response, next: NextFunction) => {
   console.log('err', err);
-  
+
   if (ERRORS[err.message]) {
     const errorDetail = ERRORS[err.message];
     res.status(err.statusCode).send(errorDetail.message);
