b6fc183342bcd4609914036c0701f0b63675d511
haeun.choi <haeunchoi.dev@gmail.com>
2023-08-16 16:46:05 +0900

    Feat: mysql dev 모드일때 디버거 허용

--- src/db/index.ts
+++ src/db/index.ts
@@ -6,7 +6,13 @@ const sql_user = process.env.SQL_USER;
 const sql_pwd = process.env.SQL_PASSWORD;
 const sql_db = process.env.SQL_DATABASE;
 
-if (sql_host === undefined || sql_port === undefined || sql_user === undefined || sql_pwd === undefined || sql_db === undefined) {
+if (
+  sql_host === undefined ||
+  sql_port === undefined ||
+  sql_user === undefined ||
+  sql_pwd === undefined ||
+  sql_db === undefined
+) {
   throw new Error('database info is undefiend');
 }
 
@@ -16,8 +22,14 @@ const pool = mysql.createPool({
   port: Number(sql_port),
   user: sql_user,
   password: sql_pwd,
-  database: sql_db
+  database: sql_db,
 });
+
+if (process.env.MODE === 'dev') {
+  pool.on('connection', (connection) => {
+    connection.config.debug = true;
+  });
+}
 
 function promiseQuery(connection, query, values) {
   return new Promise((resolve, reject) => {
