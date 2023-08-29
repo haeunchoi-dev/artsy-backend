c12cd1fd761e2f0c2ff43c08c050c4d224559226
haeun.choi <haeunchoi.dev@gmail.com>
2023-08-29 21:29:19 +0900

    feat: aws code deploy hooks ver 0.1

---
+++ scripts/install_dependencies.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+cd "/home/ubuntu/build"
+
+npm install
+
+exit 0
---
+++ scripts/move_previous.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+current_date=$(date +"%Y%m%d%H%M%S")
+backup_source="/home/ubuntu/build"
+backup_dest_base="/home/ubuntu/backup"
+
+backup_dest="$backup_dest_base/$current_date"
+
+mkdir -p $backup_dest
+
+mv $backup_source/* $backup_dest/
+
+mkdir -p $backup_source
+
+exit 0
